9559194cc62411b2acf01d821cf63e45db7f5ef9
proyectosbeta <josego85@gmail.com>
2016-11-06 07:51:15 -0600

    Se arreglo lo del Google Analitycs

--- fuentes/application/views/comunes/pie_pagina.php
+++ fuentes/application/views/comunes/pie_pagina.php
@@ -5,7 +5,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', <?php echo GOOGLE_ANALYTICS_US; ?>, <?php echo NOMBRE_DOMINIO; ?>);
+  ga('create', '<?php echo GOOGLE_ANALYTICS_US; ?>', 'auto');
   ga('send', 'pageview');
 
 </script>
